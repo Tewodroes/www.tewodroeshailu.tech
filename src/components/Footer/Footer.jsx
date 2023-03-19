6d8c93b73be64ed303be046a8711adbddcca949c
Tewodroes <teddymeselu@gmail.com>
2023-03-19 01:16:40 -0500

    First Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,354 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdAlternateEmail } from "react-icons/md";
+import {CgProfile } from "react-icons/cg";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { AiFillGithub, AiFillLinkedin, AiOutlineArrowUp } from "react-icons/ai";
+import { BsFacebook, BsSlack } from "react-icons/bs";
+import { FiMail, FiPhone, FiPhoneCall, FiFileText } from "react-icons/fi";
+import { Slide, Zoom, Fade } from "react-awesome-reveal";
+
+const Footer = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+  });
+  const [formStatus, setFormStatus] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if(!formData.name || !formData.email || !formData.subject || !formData.message){
+        setFormStatus("Please fill in all required fields");
+        return;
+      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(formData.email)) {
+        setFormStatus("Please enter a valid email address");
+        return;
+      }
+      const phoneRegex = /^\d{10}$/;
+      if (formData.phone && !phoneRegex.test(formData.phone)) {
+        setFormStatus("Please enter a valid 10-digit phone number");
+        return;
+      }
+      const response = await fetch("https://tvok5rugtk.execute-api.us-east-1.amazonaws.com/prod/contact-form", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({"name": formData.name,"email": formData.email, "subject": formData.subject, "message": formData.message}),
+      });
+      if (response.ok) {
+        setFormStatus("Message sent successfully!");
+      } else {
+        setFormStatus("Message is not sent. Please try again.");
+      }
+    } catch (error) {
+      console.error(error);
+      setFormStatus("Error sending message. Please try again.");
+    }
+  };
+  
+
+  const scrollUp = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <Container id="footer">
+
+      <Profile>
+        <Slide direction="left" delay={1}>
+          <h1>Portfolio</h1>
+        </Slide>
+        <div className="address">
+          <Slide direction="left">
+            <h1>Address:</h1>
+          </Slide>
+          <Slide direction="left">
+            <p>1000 North 4th Street Fairfield, Iowa(IA), 52557</p>
+          </Slide>
+        </div>
+        <div className="links">
+          <Slide direction="left">
+            <h1>Contact me directly:</h1>
+          </Slide>
+          <div>
+            <span>
+              <FiPhoneCall />
+            </span>
+            <Slide direction="left">
+              <a href="tel:+16412332850">+1 641 233 2850</a>
+            </Slide>
+          </div>
+          <div>
+            <Slide direction="left">
+              <span>
+                <HiOutlineMailOpen />
+              </span>
+            </Slide>
+            <Slide>
+              <a href="tewodroeshailu@gmail.com">tewodroeshailu@gmail.com</a>
+            </Slide>
+          </div>
+        </div>
+        <div className="profiles">
+          <Slide direction="left">
+            <h1>Check my profiles</h1>
+          </Slide>
+          <div className="icons">
+            <Zoom>
+              <span>
+                <a href="/">
+                  <AiFillGithub />
+                </a>
+              </span>
+            </Zoom>
+            <Zoom>
+              <span>
+                <a href="/">
+                  <AiFillLinkedin />
+                </a>
+              </span>
+            </Zoom>
+            <Zoom>
+              <span>
+                <a href="/">
+                  <BsFacebook />
+                </a>
+              </span>
+            </Zoom>
+            <Zoom>
+              <span>
+                <a href="/">
+                  <BsSlack />
+                </a>
+              </span>
+            </Zoom>
+          </div>
+        </div>
+        <Fade>
+          <ArrowUp onClick={scrollUp}>
+            <AiOutlineArrowUp />
+          </ArrowUp>
+        </Fade>
+
+      </Profile>
+      <Form>
+        <Slide direction="right">
+          <form>
+            <div className="name">
+              <span>
+                <CgProfile />
+              </span>
+              <input type="text" placeholder="Fullname..." name="name" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div className="phone">
+              <span>
+                <FiPhone />
+              </span>
+              <input type="tel" placeholder="Phone..." name="phone" value={formData.phone} onChange={handleInputChange} />
+            </div>
+            <div className="email">
+              <span>
+                <MdAlternateEmail />
+              </span>
+              <input type="email" placeholder="Email..." name="email" value={formData.email} onChange={handleInputChange} />
+            </div>
+            <div className="subject">
+              <span>
+                <FiFileText />
+              </span>
+              <input type="text" placeholder="Subject..." name="subject" value={formData.subject} onChange={handleInputChange} />
+            </div>            
+            <div className="message">
+              <span className="messageIcon">
+                <FiMail />
+              </span>
+              <textarea cols="30" rows="10" placeholder="Message..." name="message" value={formData.message} onChange={handleInputChange}></textarea>
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
+            {formStatus && <p>{formStatus}</p>}
+          </form>
+        </Slide>
+      </Form>
+    </Container>
+  );
+}
+
+export default Footer;
+
+const Container = styled.div`
+  margin-top: 2rem;
+  position: relative;
+  padding: 2rem 0;
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+    gap: 3rem;
+  }
+`;
+const Profile = styled.div`
+  flex: 1;
+  .address {
+    padding: 1rem 0;
+    h1 {
+      font-size: 1.2rem;
+    }
+
+    p {
+      width: 60%;
+      padding-top: 0.5rem;
+      @media (max-width: 650px) {
+        width: 100%;
+      }
+    }
+  }
+
+  .links {
+    h1 {
+      font-size: 1.2rem;
+      margin-bottom: 0.5rem;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      a {
+        text-decoration: none;
+        color: lightgray;
+        :hover {
+          color: orange;
+        }
+      }
+    }
+  }
+
+  .profiles {
+    h1 {
+      font-size: 1.2rem;
+      padding: 1rem 0;
+    }
+
+    .icons {
+      display: flex;
+      align-items: center;
+
+      span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #000;
+        width: 2rem;
+        height: 2rem;
+        margin-right: 0.5rem;
+        border-radius: 50px;
+
+        :hover {
+          background-color: orange;
+        }
+
+        a {
+          margin-top: 0.2rem;
+          color: #fff;
+        }
+      }
+    }
+  }
+`;
+const ArrowUp = styled.div`
+  width: 2rem;
+  height: 2rem;
+  background-color: #01be96;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 1.3rem;
+  font-weight: 700;
+  margin-top: 2rem;
+  @media (max-width: 650px) {
+    position: absolute;
+    right: 3rem;
+    top: 16rem;
+  }
+`;
+const Form = styled.div`
+  flex: 1;
+  h1 {
+    font-size: 1.3rem;
+    padding-bottom: 0.7rem;
+  }
+
+  form {
+    background-color: #191923;
+    padding: 0.8rem;
+    border-radius: 5px;
+    .name,
+    .email,
+    .message,
+    .phone,
+    .subject {
+      display: flex;
+      border: 1px solid gray;
+      margin-bottom: 0.5rem;
+      input,
+      textarea {
+        width: 100%;
+        border: none;
+        outline: none;
+        color: #fff;
+        background-color: transparent;
+        padding: 1rem 0.5rem;
+      }
+      span {
+        background-color: #3e3e3e;
+        width: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .messageIcon {
+        align-items: flex-start;
+        padding-top: 0.5rem;
+      }
+    }
+
+    button {
+      width: 5rem;
+      height: 1.8rem;
+      background-color: #01be96;
+      border: none;
+      border-radius: 5px;
+      filter: drop-shadow(0px 4px 5px #01be9551);
+      cursor: pointer;
+      :hover {
+        filter: drop-shadow(0px 6px 9px #01be9551);
+      }
+    }
+  }
+`;
